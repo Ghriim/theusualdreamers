8988a94c20b5269d0fbb1c9346c08aead3738da7
Quentin Vignier <vignierquentin@yahoo.fr>
2017-05-17 19:33:11 +0200

    [CONF] Add prod configuration

---
+++ app/config/parameters_prod.php
@@ -0,0 +1,17 @@
+<?php
+
+$container->setParameter('database_host', getenv('DATABASE_HOST'));
+$container->setParameter('database_port', getenv('DATABASE_PORT'));
+$container->setParameter('database_name', getenv('DATABASE_NAME'));
+$container->setParameter('database_user', getenv('DATABASE_USER'));
+$container->setParameter('database_password', getenv('DATABASE_PASSWORD'));
+
+$container->setParameter('secret', getenv('SECRET'));
+
+$container->setParameter('mailer_host', getenv('MAILER_HOST'));
+$container->setParameter('mailer_username', getenv('MAILER_USER'));
+$container->setParameter('mailer_password', getenv('MAILER_PASSWORD'));
+$container->setParameter('mailer_port', getenv('MAILER_PORT'));
+$container->setParameter('mailer_dev_recipient', getenv('MAILER_DEV_RECIPIENT') ? getenv('MAILER_DEV_RECIPIENT') : null);
+
+$container->setParameter('front_url', getenv('FRONT_URL'));
