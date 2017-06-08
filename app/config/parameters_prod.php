da334dd6e9be2ea897741675ce6a4e4ba061af0a
Quentin Vignier <vignierquentin@yahoo.fr>
2017-06-08 07:44:04 +0200

    [HOTFIX] Emails issue

--- app/config/parameters_prod.php
+++ app/config/parameters_prod.php
@@ -9,11 +9,7 @@ $container->setParameter('database_password', getenv('DATABASE_PASSWORD'));
 
 $container->setParameter('secret', getenv('SECRET'));
 
-$container->setParameter('mailer_transport', 'smtp');
-$container->setParameter('mailer_host', getenv('MAILER_HOST'));
 $container->setParameter('mailer_username', getenv('MAILER_USER'));
 $container->setParameter('mailer_password', getenv('MAILER_PASSWORD'));
-$container->setParameter('mailer_port', getenv('MAILER_PORT'));
-$container->setParameter('mailer_dev_recipient', getenv('MAILER_DEV_RECIPIENT') ? getenv('MAILER_DEV_RECIPIENT') : null);
 
 $container->setParameter('root_url', getenv('ROOT_URL'));
